b5e94aafc225fa318bfded4f397db800fa84a47c
Tero @RC-Thoughts.com <tero@rc-thoughts.com>
2016-12-24 10:15:10 +0200

    v.1.0
    Initial release

---
+++ RCT-12h.lua
@@ -0,0 +1,53 @@
+--[[
+	---------------------------------------------------------
+    RCT Clock is a clock to display time in 12-hour format
+	
+	Both big and small window available for main screen.
+	
+	For DC/DS-16/24. No settings required.
+	---------------------------------------------------------
+	RCT Clock is part of RC-Thoughts Jeti Tools.
+	---------------------------------------------------------
+	Released under MIT-license by Tero @ RC-Thoughts.com 2016
+	---------------------------------------------------------
+--]]
+----------------------------------------------------------------------
+-- Locals for the application
+local timeOrg, timeHour, timeDisp, timeSym
+----------------------------------------------------------------------
+-- Draw the telemetry windows
+local function printClock(width, height)
+	if(height > 25) then
+		lcd.drawText((150 - lcd.getTextWidth(FONT_BIG,timeDisp))/2,22,timeDisp,FONT_BIG)
+		else
+		lcd.drawText(150 - lcd.getTextWidth(FONT_BOLD,timeDisp),2,timeDisp,FONT_BOLD)
+	end
+end
+----------------------------------------------------------------------
+local function loop()
+	local timeOrg = system.getDateTime()
+	local timeHour = tonumber(timeOrg.hour)
+	if(timeHour == 12) then
+		timeSym = "pm"
+	end
+	if(timeHour < 12) then
+		timeSym = "am"
+	end
+	if(timeHour == 0) then
+		timeHour  = 12
+		timeSym = "am"
+	end
+	if(timeHour >= 13) then
+		timeHour = timeHour - 12
+		timeSym = "pm"
+	end
+	timeDisp = ((string.format("%d:%02d:%02d", timeHour, timeOrg.min, timeOrg.sec, timeSym)).. " " .. timeSym)
+end
+----------------------------------------------------------------------
+-- Application initialization
+local function init()
+	system.registerTelemetry(1,"Clock",0,printClock)
+end
+----------------------------------------------------------------------
+ampmVersion = "v.1.0"
+return {init=init, loop=loop, author="RC-Thoughts", version=ampmVersion, name="RCT Clock"}
---
+++ README.md
@@ -0,0 +1,16 @@
+# LUA 12h Clock
+Jeti LUA application to display time on main screen in 12-hour format with am/pm symbol
+
+Works in DC/DS-16/24
+
+Installation:
+- Copy RCT-12h.lua to transmitters SD-card to /Apps folder
+- Run application in transmitter
+- Select telemetry control "Clock" to your screen
+- No settings needed
+
+Current info available on https://www.rc-thoughts.com/2016/08/rc-thoughts-tools/
+
+Tero from RC-Thoughts 2016
+
+![Display-picture](https://www.rc-thoughts.com/wp-content/uploads/2016/12/RCT-12h_001.png)
